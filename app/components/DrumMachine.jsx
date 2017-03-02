d4d80b93a0b6b3b7302e988f468efcd3df1222e1
Mithun <mselvaratnam@gmail.com>
2017-03-02 15:17:10 -0500

    feat(drumMachine): built up drumMachine component and added tempo control

--- app/components/DrumMachine.jsx
+++ app/components/DrumMachine.jsx
@@ -1,23 +1,82 @@
 import React, { Component } from 'react';
-import { drums, triggerDrums } from '../audio_scripts/drums';
+import Tone from 'tone';
+import { connect } from 'react-redux';
+import Dial from './Dial.jsx';
+import { setTempo } from '../ducks/drum_ducks.jsx';
+
+let drums;
 
 export class DrumMachine extends Component {
-  constructor(){
-    super()
-    // this.state = {
-    //   drums: {}
-    // }
+  constructor(props){
+    super(props)
+
+    drums = new Tone.MultiPlayer({
+    urls : {
+      "hihat0" : "../samples/hihat.wav",
+      "hihat1" : "../samples/hihat2.wav",
+      "hihat2" : "../samples/hihat3.wav",
+      "snare0": "../samples/snare.wav",
+      "snare1" : "../samples/snare2.wav",
+      "snare2" : "../samples/snare3.wav",
+      "kick0": "../samples/kick.wav",
+      "kick1" : "../samples/kick2.wav",
+      "kick2" : "../samples/kick3.wav"
+    },
+    volume : -48,
+    fadeOut : 0.1,
+  }).toMaster();
+
+    Tone.Transport.bpm.value = drums.tempo;
   }
+
+  // function must change to handle multiple rows
+  triggerDrums(drumMatrix, time, col){
+    let column = drumMatrix.matrix[col];
+    for (let i = 0; i < column.length; i++) {
+      if (column[0] === 1) {
+        drums.start('hihat' + audioSettings.hihat, time, 0, "16n", 0, audioSettings.hihatvol)
+      }
+      if (column[1] === 1) {
+        drums.start('snare' + audioSettings.snare, time, 0, "16n", 0, audioSettings.snarevol)
+      }
+      if (column[2] === 1) {
+        drums.start('kick' + audioSettings.kick, time, 0, "16n", 0, audioSettings.kickvol)
+      }
+    }
+    drumMatrix.place = col;
+  }
+
+  changeTempo(tempo){
+    Tone.Transport.bpm.value = tempo;
+  }
+
   render(){
+    const { nxDefine } = this.props;
     return (
       <div>
         <canvas
           data-type="matrix"
           id="drumMatrix2"
-          label="Drum Machine"
-          ref={(canvas) => {this.props.nxDefine(canvas)}}>
+          ref={(canvas) => {nxDefine(canvas)}}>
           </canvas>
+        <Dial nxDefine={nxDefine}
+              changeRouter={this.changeTempo}
+              dispatcher={this.props.setTempo}
+              range={['60', '200']}
+              args={[]}
+              id='tempoMod'
+               />
       </div>
     )
   }
 }
+
+// make tempo set function and dispatch/redux for drums
+
+/* REDUX CONTAINER */
+const mapStateToProps = ({ drums }) => ({ drums })
+const mapDispatchToProps = dispatch => ({
+  setTempo: tempo => dispatch(setTempo(tempo))
+ })
+
+export default connect(mapStateToProps,mapDispatchToProps)(DrumMachine);
